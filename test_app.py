f4f9ca85ea826ead25e7017ce37f40648dcb7c32
Itay Brandes <Brandes.Itay@gmail.com>
2019-02-24 07:46:55 +0200

    more stuff

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+import time
+
+class TestPyServer(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_dependencies(self):
+        self.assertTrue(sys.version_info >= (3, 4))
+    def test_dummy(self):
+        time.sleep(3)
+        self.assertTrue(True)
+    def test_dummy2(self):
+        time.sleep(1)
+        self.assertTrue(True)
